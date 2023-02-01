cf7c76992201b14fdcbab12c21526974451e1037
kiman21 <kim.andrew0121@gmail.com>
2023-01-31 22:40:47 -0800

    first-version

---
+++ Develop/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.send('Navigate to /index or /notes'));
+
+app.get('/index', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
